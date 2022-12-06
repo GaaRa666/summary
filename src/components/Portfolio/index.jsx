a0828e0a6241e43e43df82634ba9b285888075ef
GaaRa666 <afonkin.leha@gmail.com>
2022-12-06 15:12:56 +0500

    link

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -6,19 +6,19 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className="portfolio__skill">
         <span>-</span>
-        <p>Bootstrap 4 Material Design (Sample Link)</p>
+        <p className="link">Bootstrap 4 Material Design (Sample Link)</p>
       </div>
       <div className="portfolio__skill">
         <span>-</span>
-        <p>Modern JavaScript stack</p>
+        <p className="link">Modern JavaScript stack</p>
       </div>
       <div className="portfolio__skill">
         <span>-</span>
-        <p>Datepicker for twitter bootstrap</p>
+        <p className="link">Datepicker for twitter bootstrap</p>
       </div>
       <div className="portfolio__skill">
         <span>-</span>
-        <p>Fast and reliable Bootstrap widgets in Angular </p>
+        <p className="link">Fast and reliable Bootstrap widgets in Angular </p>
       </div>
     </div>
   );
