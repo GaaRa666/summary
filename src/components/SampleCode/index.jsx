d505d64df41dcdd36ca583e5b28b25b3726faafc
GaaRa666 <afonkin.leha@gmail.com>
2022-12-09 14:51:17 +0500

    SampleCode

--- src/components/SampleCode/index.jsx
+++ src/components/SampleCode/index.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import "./style.css";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atelierForestLight } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const SampleCode = () => {
-  const sampleCodeExample = ` 
-  <div class="golden-grid">
-    <div
-      style="grid-area:
+  const sampleCodeExample = ` <div class="golden-grid">
+    <div style="grid-area:
         11 / 1 / span 10 / span 
         12;">
     </div>
-  </div> 
-`;
+  </div> `;
   return (
     <div className="sampleCode">
       <h2>Sample code</h2>
-      <pre>{sampleCodeExample}</pre>
+      <SyntaxHighlighter
+        children={sampleCodeExample}
+        language="htmlbars"
+        style={atelierForestLight}
+        showLineNumbers
+        lineNumberContainerStyle
+      />
     </div>
   );
 };
